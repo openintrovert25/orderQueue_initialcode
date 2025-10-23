b7300ee327a11e7c2f93e65c00104d20361cf58b
IJAG <ijagarciaqt@gmail.com>
2025-10-23 23:17:26 +0800

    with header and footer

--- components/headers/header.php
+++ components/headers/header.php
@@ -5,7 +5,7 @@
         <ul class="nav-links animated-tabs">
             <li><a href="index.php" class="<?= ($page === 'home') ? 'active' : '' ?>"><span>Home</span></a></li>
             <li><a href="rider.php" class="<?= ($page === 'rider') ? 'active' : '' ?>"><span>Rider</span></a></li>
-            <li><a href="stocks.php" class="<?= ($page === 'stocks') ? 'active' : '' ?>"><span>Stocks</span></a></li>
+
             <li><a href="stocks.php" class="<?= ($page === 'stocks') ? 'active' : '' ?>"><span>Stocks</span></a></li>
             <li><button class="signin-btn"><span> Sign in</span></button></li>
             <li><i class="fa-solid fa-cart-shopping cart-icon" id="cart-btn"></i></li>
